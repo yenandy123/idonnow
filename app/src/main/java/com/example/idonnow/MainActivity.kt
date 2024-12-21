44277d506603e58921d5d21a61c2d762892a102a
鄒忠伯 <11110153@chihlee.edu.tw>
2024-12-21 22:12:10 +0800

    準備讓按鈕跳轉至光感測器

--- app/src/main/java/com/example/idonnow/MainActivity.kt
+++ app/src/main/java/com/example/idonnow/MainActivity.kt
@@ -2,18 +2,35 @@ package com.example.idonnow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.fragment.app.commit
 import com.example.idonnow.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
+    private val dataViewModel:DataViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+
+
+
+
+
+
+
         supportFragmentManager.commit {
             replace(R.id.showfragment,BlankFragment())
+        }
+        binding.button2.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.showfragment,light())
+                .commit()
+
         }
     }
 }
