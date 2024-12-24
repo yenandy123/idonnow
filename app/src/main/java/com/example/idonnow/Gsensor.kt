c3921320af282a8e4f458d27d2fa117360fdc7d7
鄒忠伯 <11110153@chihlee.edu.tw>
2024-12-24 13:26:35 +0800

    陀螺儀製作

---
+++ app/src/main/java/com/example/idonnow/Gsensor.kt
@@ -0,0 +1,11 @@
+package com.example.idonnow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Gsensor : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gsensor)
+    }
+}
